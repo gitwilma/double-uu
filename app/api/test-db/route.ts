2ad24d25d7350cca52d139eb92ee20c674fee77d
gitwilma <wilma.haakansson@gmail.com>
2025-11-19 15:36:56 +0100

    Initial setup with nextjs and mongodb

---
+++ app/api/test-db/route.ts
@@ -0,0 +1,17 @@
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db();
+
+    const collections = await db.collections();
+    const collectionNames = collections.map((c) => c.collectionName);
+
+    return NextResponse.json({ success: true, collections: collectionNames });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ success: false, error: "Could not connect to DB" });
+  }
+}
